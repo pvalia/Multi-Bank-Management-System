387c0a73763ead3e05dc02d7c5c159aa378c8c10
pvalia <pvaliakhmetova@gmail.com>
2024-03-08 14:00:40 -0500

    update

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,64 +1,36 @@
-import { useEffect } from "react";
-import {Routes,Route,useNavigationType,useLocation,} from "react-router-dom";
-
-import EmployeeForm from "./pages/EmployeeForm";
-import BranchForm from "./pages/BranchForm";
-import HomePage from "./pages/HomePage";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from './pages/HomePage';
+import EmployeeForm from './pages/EmployeeForm';
+import BranchForm from './pages/BranchForm';
 
 function App() {
-  const action = useNavigationType();
-  const location = useLocation();
-  const pathname = location.pathname;
-
-  useEffect(() => {
-    if (action !== "POP") {
-      window.scrollTo(0, 0);
-    }
-  }, [action, pathname]);
-
-  useEffect(() => {
-    let title = "";
-    let metaDescription = "";
-
-    switch (pathname) {
-      case "/":
-        title = "";
-        metaDescription = "";
-        break;
-      case "/EmployeeForm":
-        title = "";
-        metaDescription = "";
-        break;
-      case "/BranchForm":
-        title = "";
-        metaDescription = "";
-        break;
-      default:
-        title = "";
-        metaDescription = "";
-        break;
-    }
-    
-    if (title) {
-      document.title = title;
-    }
-
-    if (metaDescription) {
-      const metaDescriptionTag = document.querySelector(
-        'head > meta[name="description"]'
-      );
-      if (metaDescriptionTag) {
-        metaDescriptionTag.content = metaDescription;
-      }
-    }
-  }, [pathname]);
-
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/EmployeeForm" element={<EmployeeForm />} />
-      <Route path="/BranchForm" element={<BranchForm />} />
-    </Routes>
+    <Router>
+      <div>
+        {/* Navigation Links Example (optional) */}
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/create-employee">Create Employee</a>
+            </li>
+            <li>
+              <a href="/create-branch">Create Branch</a>
+            </li>
+          </ul>
+        </nav>
+        {/* Routes Setup */}
+        <Routes>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/create-employee" component={EmployeeForm} />
+          <Route path="/create-branch" component={BranchForm} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
 export default App;
