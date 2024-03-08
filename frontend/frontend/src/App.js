fc34f467b3c197cd6c252682df313bfbeb3e8e09
pvalia <pvaliakhmetova@gmail.com>
2024-03-08 13:34:39 -0500

    created pages

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import {Routes,Route,useNavigationType,useLocation,} from "react-router-dom";
+
+import EmployeeForm from "./pages/EmployeeForm";
+import BranchForm from "./pages/BranchForm";
+import HomePage from "./pages/HomePage";
 
 function App() {
+  const action = useNavigationType();
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  useEffect(() => {
+    if (action !== "POP") {
+      window.scrollTo(0, 0);
+    }
+  }, [action, pathname]);
+
+  useEffect(() => {
+    let title = "";
+    let metaDescription = "";
+
+    switch (pathname) {
+      case "/":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/EmployeeForm":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/BranchForm":
+        title = "";
+        metaDescription = "";
+        break;
+      default:
+        title = "";
+        metaDescription = "";
+        break;
+    }
+    
+    if (title) {
+      document.title = title;
+    }
+
+    if (metaDescription) {
+      const metaDescriptionTag = document.querySelector(
+        'head > meta[name="description"]'
+      );
+      if (metaDescriptionTag) {
+        metaDescriptionTag.content = metaDescription;
+      }
+    }
+  }, [pathname]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/EmployeeForm" element={<EmployeeForm />} />
+      <Route path="/BranchForm" element={<BranchForm />} />
+    </Routes>
   );
 }
-
 export default App;
